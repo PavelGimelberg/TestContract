291069a0e9c38ea7d854ab1de404ab004810a8a4
PavelGimelberg <pavelgbg@gmail.com>
2023-03-31 01:02:16 +1100

    Final changes: Contract updates, tests, and deployment configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,25 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan"); 
+
+const { projectId, privateKey, etherscanApiKey } = require('./secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
       chainId: 1337
-    }
-  }
+    },
+    fantomTestnet: {
+      url: `https://rpc.testnet.fantom.network`,
+      chainId: 4002,
+      gasPrice: 22000000000,
+      accounts: [privateKey],
+    },
+  },
+  etherscan: {
+     apiKey: etherscanApiKey,
+     chainId: 4002
+  },
 };
+
